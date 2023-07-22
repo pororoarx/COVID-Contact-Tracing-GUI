d34db705be2298579ab1f53ccb6d0d92f7e06920
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-22 11:50:54 +0800

    pseudocode for the main program

---
+++ main_program.py
@@ -0,0 +1,14 @@
+# import tkinter
+# import entry functions (add and search)
+
+# main window
+
+# create the instance of a class for add entry
+# create the instance of a class for search entry
+
+# create a button for the add entry
+# place the add entry button in the middle
+# create a button for the search entry
+# place the search entry in the middle
+
+# call the main loop to start the app
