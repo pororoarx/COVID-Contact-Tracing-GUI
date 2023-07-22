1fa9c70061bece2078edd9143cb9a538303efe6b
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-22 13:41:36 +0800

    pseudocode for add entry function

---
+++ add_entry.py
@@ -0,0 +1,65 @@
+# import all the required modules (csv, tkinter, mesagebox, re, font, etc.)
+
+# define the Create class
+        # initiliaze main window 
+        # BooleanVar for data privacy agreement
+
+    # define the function for add entry
+        # new window for add entry
+
+        # Set the title for personal information entry
+
+        # Add entry field for first name
+        # Add entry field for last name
+        # Add entry field for middle name
+        # Add entry field for age
+        # Add entry field for address
+        # Add entry field for email
+        # Add entry field for contact
+        # if minor:
+        # Add entry field for guardian's name 
+        # Add entry field for guardian's contact number
+        # Add entry field for their relationship
+
+        # Set the title for health declaration form entry
+
+        # question 1 - vaccination
+        # set radio button for q1
+        # question 2 - symptoms
+        # set checkbutton for q2
+        # question 3 - exposure
+        # set radio button for q3
+        # question 4 - contact
+        # set radio button for q4
+        # question 5 - positive
+        # set radio button for q5
+        # question 6 - places
+        # Add entry field for places q6
+
+        # Set the title for data privacy section
+
+        # add a text box for the data privacy agreememt
+        # set checkbutton for data privacy
+
+        # add the submit button 
+
+        # save the window ref
+
+
+    # define a function for contact infos
+        # collect all inputs of user in every field
+
+        # add error message if required fields were not answered
+
+            # add exception handling for age
+            # handle errors like missing field for guardian's info if user is minor
+
+                # open csv file to to save all inputs of user
+
+                # print a message every time a new user submit a file
+
+        # add error message if data privacy is not checked
+
+        # close the window
+
+
