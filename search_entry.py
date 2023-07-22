72eb87c4dae82f05ad2a9a0b853e3cf02bd02e27
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-23 02:13:03 +0800

    add pseudocode for search entry

---
+++ search_entry.py
@@ -0,0 +1,41 @@
+# import necessary modules
+
+# create a class for search entry
+    # set constructor for the class 
+
+    # define a function to search for a contact information
+        # create a new window for searching
+
+        # set label for asked input and add field  
+
+        # add the button to start search
+
+    # define a function to read the information from CSV file
+        # take the entered input from the search widget
+        # open csv file and store its value in data
+        # create the list of all the names from the data
+
+        # execute the following code if name exists in the list
+            # if name is available, display the information
+                    # create a mew window for the information of searched user
+
+                    # set labels for every contact information
+                    # name
+                    # age
+                    # address
+                    # email
+                    # contact
+                    # guardian name
+                    # guardian contact
+                    # relationship
+                    # question 1
+                    # question 2
+                    # question 3
+                    # question 4
+                    # question 5
+                    # question 6
+
+            # if the name is not found, display error message
+
+
+  
