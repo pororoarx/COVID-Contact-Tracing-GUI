329e04c27b21d8b5101547b0e26170158bac620b
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-22 17:10:12 +0800

    create button for add entry

--- main_program.py
+++ main_program.py
@@ -13,6 +13,7 @@ instance_create = Create(main)
 # create the instance of a class for search entry
 
 # create a button for the add entry
+create_button = tkinter.Button(main, text="Add entry", command=instance_create.create_contact, width=10, height=1, font=("Times New Roman", 12),bg="#87CEEB")
 # place the add entry button in the middle
 # create a button for the search entry
 # place the search entry in the middle
