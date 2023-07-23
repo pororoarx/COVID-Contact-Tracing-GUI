083d4939b1a70d9aea5dfeb96785a97147d4caed
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-23 15:05:08 +0800

    added bg and backdrop to add entry window

--- add_entry.py
+++ add_entry.py
@@ -19,18 +19,22 @@ class Create:
         window = tkinter.Toplevel(self.main)
         # new window for add entry
         window.title("Add a new contact")
-        window.geometry("1000x600")
+        window.geometry("1100x750")
+        # set the background color
+        window.configure(bg="#ADD8E6")
+        backdrop = tkinter.Frame(window, bg="white", width=1075, height=725)
+        backdrop.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
         # Set the title for personal information entry
         bold_font = font.Font(weight="bold", size=11)
         name = tkinter.Label(window, text=" I. Personal Information", font=bold_font)
-        name.grid(row=0, column=0, columnspan=3, sticky="w")
+        name.grid(row=0, column=0, columnspan=3, sticky="w", padx=20, pady=20)
 
         # Add entry field for first name
         first_name = tkinter.Label(window, text="First Name")
         first_name.grid(row=1, column=0, sticky="e")
         self.first_name_entry = tkinter.Entry(window)
-        self.first_name_entry.grid(row=1, column=1)
+        self.first_name_entry.grid(row=1, column=1, sticky="w")
 
         # Add entry field for last name
         last_name = tkinter.Label(window, text="Last Name")
@@ -48,7 +52,7 @@ class Create:
         age = tkinter.Label(window, text="Age")
         age.grid(row=2, column=0, sticky="e")
         self.age_entry = tkinter.Entry(window)
-        self.age_entry.grid(row=2, column=1)
+        self.age_entry.grid(row=2, column=1, sticky="w")
 
         # Add entry field for address
         address = tkinter.Label(window, text="Address")
@@ -66,7 +70,7 @@ class Create:
         contact = tkinter.Label(window, text="Contact No.:")
         contact.grid(row=3, column=0, sticky="e")
         self.contact_entry = tkinter.Entry(window)
-        self.contact_entry.grid(row=3, column=1)
+        self.contact_entry.grid(row=3, column=1, sticky="w")
 
         # if minor:
         # Add entry field for guardian's name 
@@ -85,16 +89,16 @@ class Create:
         relationship_name = tkinter.Label(window, text="Relationship:")
         relationship_name.grid(row=4, column=0, sticky="e")
         self.relationship_entry = tkinter.Entry(window)
-        self.relationship_entry.grid(row=4, column=1)
+        self.relationship_entry.grid(row=4, column=1, sticky="w")
 
         # Set the title for health declaration form entry
         bold_font = font.Font(weight="bold", size=11)
         name = tkinter.Label(window, text=" II. Health Declaration Form", font=bold_font)
-        name.grid(row=7, column=0, columnspan=3, sticky="w")
+        name.grid(row=7, column=0, columnspan=3, sticky="w", padx=20, pady=20)
 
         # question 1 - vaccination
         question_1 = tkinter.Label(window, text="Have you been vaccinated for COVID-19?")
-        question_1.grid(row=8, column=0, sticky="w")
+        question_1.grid(row=8, column=0, sticky="w", padx=25)
         # set radio button for q1
         self.vaccination_var = tkinter.StringVar()
         # default selection
@@ -102,11 +106,11 @@ class Create:
         vaccinations = ["Not yet", "1st dose", "2nd Dose"]
         for i, vaccination in enumerate(vaccinations):
             radiobutton = tkinter.Radiobutton(window, text=vaccination, variable=self.vaccination_var, value=vaccination)
-            radiobutton.grid(row=9, column=0+i, columnspan=3, sticky="w")
+            radiobutton.grid(row=9, column=0+i, columnspan=3, sticky="w", padx=25)
 
         # question 2 - symptoms
         question_2 = tkinter.Label(window, text="Are you experiencing any symptoms for the past 14 days? (Check all that apply. If none, leave it blank)")
-        question_2.grid(row=12, column=0, columnspan=3, sticky="w")
+        question_2.grid(row=12, column=0, columnspan=3, sticky="w", padx=25)
         # set checkbutton for q2
         self.symptoms_var1 = tkinter.BooleanVar()
         self.symptoms_var2 = tkinter.BooleanVar()
@@ -120,11 +124,11 @@ class Create:
         symptoms_options = ["Fever", "Cough", "Sore throat", "Loss of taste/smell"]
         for i, symptom in enumerate(symptoms_options):
             checkbutton = tkinter.Checkbutton(window, text=symptom, variable=self.symptoms_var1 if i == 0 else self.symptoms_var2 if i == 1 else self.symptoms_var3 if i == 2 else self.symptoms_var4)
-            checkbutton.grid(row=13, column=0+i, columnspan=3, sticky="w")
+            checkbutton.grid(row=13, column=0+i, columnspan=3, sticky="w", padx=25)
 
         # question 3 - exposure
         question_3 = tkinter.Label(window, text="Have you had exposure to a probable or confirmed case of COVID-19 in the last 14 days?")
-        question_3.grid(row=16, column=0, columnspan=3, sticky="w")
+        question_3.grid(row=16, column=0, columnspan=3, sticky="w", padx=25)
         # set radio button for q3
         self.exposure_var = tkinter.StringVar()
         self.exposure_var.set("Not yet")  
@@ -132,11 +136,11 @@ class Create:
         exposure = ["No", "Yes", "Not sure"]
         for i, options in enumerate(exposure):
             radiobutton = tkinter.Radiobutton(window, text=options, variable=self.exposure_var, value=options)
-            radiobutton.grid(row=17, column=0+i, columnspan=3, sticky="w")
+            radiobutton.grid(row=17, column=0+i, columnspan=3, sticky="w", padx=25)
 
         # question 4 - contact
         question_4 = tkinter.Label(window, text="Have you been in contact with somebody who has symptoms in the last 14 days?")
-        question_4.grid(row=19, column=0, columnspan=3, sticky="w")
+        question_4.grid(row=19, column=0, columnspan=3, sticky="w", padx=25)
         # set radio button for q4
         self.contact_var = tkinter.StringVar()
         self.contact_var.set("Not yet")  
@@ -144,29 +148,29 @@ class Create:
         in_contact = ["No", "Yes"]
         for i, options in enumerate(in_contact):
             radiobutton = tkinter.Radiobutton(window, text=options, variable=self.contact_var, value=options)
-            radiobutton.grid(row=20, column=0+i, columnspan=3, sticky="w")
+            radiobutton.grid(row=20, column=0+i, columnspan=3, sticky="w", padx=25)
 
         # question 5 - positive
         question_5 = tkinter.Label(window, text="Have you been tested positive for COVID-19?")
-        question_5.grid(row=22, column=0, columnspan=3, sticky="w")
+        question_5.grid(row=22, column=0, columnspan=3, sticky="w", padx=25)
         # set radio button for q5
         self.positive_var = tkinter.StringVar()
         self.positive_var.set("Not yet")  # Default selection
         positive_test = ["No", "Yes"]
         for i, options in enumerate(positive_test):
             radiobutton = tkinter.Radiobutton(window, text=options, variable=self.positive_var, value=options)
-            radiobutton.grid(row=23, column=0+i, columnspan=3, sticky="w")
+            radiobutton.grid(row=23, column=0+i, columnspan=3, sticky="w", padx=25)
 
         # question 6 - places
         question_6 = tkinter.Label(window, text="What places have you been (besides of your home) in the last 14 days?")
         # Add entry field for places q6
-        question_6.grid(row=24, column=0, sticky="e")
+        question_6.grid(row=24, column=0, sticky="e", padx=25)
         self.question_6_entry = tkinter.Entry(window)
-        self.question_6_entry.grid(row=24, column=1)
+        self.question_6_entry.grid(row=24, column=1, padx=25)
 
         # Set the title for data privacy section
         data_privacy = tkinter.Label(window, text=" III. Data Privacy", font=bold_font)
-        data_privacy.grid(row=25, column=0, columnspan=3, sticky="w")
+        data_privacy.grid(row=25, column=0, columnspan=3, sticky="w", padx=20, pady=20)
 
         data_privacy_message = """DATA PRIVACY ACT
 
@@ -174,12 +178,12 @@ class Create:
         """
 
         # add a text box for the data privacy agreememt
-        data_privacy_text = tkinter.Text(window, width=80, height=5, wrap=tkinter.WORD)
+        data_privacy_text = tkinter.Text(window, width=90, height=7, wrap=tkinter.WORD)
         data_privacy_text.insert(tkinter.END, data_privacy_message)
-        data_privacy_text.grid(row=26, column=0, columnspan=6)
+        data_privacy_text.grid(row=26, column=0, columnspan=6, padx=25)
         # set checkbutton for data privacy
         data_privacy_agree = tkinter.Checkbutton(window, text="I agree to the terms and conditions of the data privacy notice", variable=self.data_privacy_agree)
-        data_privacy_agree.grid(row=27, column=0)
+        data_privacy_agree.grid(row=27, column=0, padx=25)
 
         # add the submit button 
         submit_button = tkinter.Button(window, text="Submit", command=self.contact_info)
