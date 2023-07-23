0f486d81d71ec5791604be3054b24d3e73f5fea2
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-23 14:15:26 +0800

    added bg and backdrop for the main window

--- main_program.py
+++ main_program.py
@@ -8,6 +8,9 @@ from search_entry import Read
 main = tkinter.Tk()
 main.title("COVID Contact Tracing")
 main.geometry("400x300")
+main.configure(bg="#ADD8E6")
+backdrop = tkinter.Frame(main, bg="white", width=370, height=270)
+backdrop.place(relx=0.5, rely=0.5, anchor=tkinter.CENTER)
 
 # create the instance of a class for add entry
 instance_create = Create(main)
@@ -15,12 +18,12 @@ instance_create = Create(main)
 instance_search = Read(main)
 
 # create a button for the add entry
-create_button = tkinter.Button(main, text="Add entry", command=instance_create.create_contact, width=10, height=1, font=("Times New Roman", 12),bg="#87CEEB")
+create_button = tkinter.Button(main, text="Add entry", command=instance_create.create_contact, width=10, height=1, font=("Times New Roman", 12),bg="#ADD8E6")
 # place the add entry button in the middle
 create_button.place(relx=0.5, rely=0.30, anchor=tkinter.CENTER)
 
 # create a button for the search entry
-search_button = tkinter.Button(main, text="Search entry", command=instance_search.search_contact, width=10, height=1, font=("Times New Roman", 12),bg="#87CEEB")
+search_button = tkinter.Button(main, text="Search entry", command=instance_search.search_contact, width=10, height=1, font=("Times New Roman", 12),bg="#ADD8E6")
 # place the search entry in the middle
 search_button.place(relx=0.5, rely=0.60, anchor=tkinter.CENTER)
 
