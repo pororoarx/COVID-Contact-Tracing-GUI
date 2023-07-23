aaaacaf54c5395cf8de4215c9eeb725844b6e342
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-23 13:32:12 +0800

    create button for search and place it in the middle

--- main_program.py
+++ main_program.py
@@ -17,13 +17,12 @@ instance_search = Read(main)
 # create a button for the add entry
 create_button = tkinter.Button(main, text="Add entry", command=instance_create.create_contact, width=10, height=1, font=("Times New Roman", 12),bg="#87CEEB")
 # place the add entry button in the middle
-create_button.place(relx=0.5, rely=0.15, anchor=tkinter.CENTER)
+create_button.place(relx=0.5, rely=0.30, anchor=tkinter.CENTER)
 
 # create a button for the search entry
+search_button = tkinter.Button(main, text="Search entry", command=instance_search.search_contact, width=10, height=1, font=("Times New Roman", 12),bg="#87CEEB")
 # place the search entry in the middle
+search_button.place(relx=0.5, rely=0.60, anchor=tkinter.CENTER)
 
 # call the main loop to start the app
 main.mainloop()
-
-
-# testing if working...
