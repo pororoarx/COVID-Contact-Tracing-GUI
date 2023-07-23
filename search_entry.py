2350bfbc31250c3959de62764c99ef1fa8b287a6
pororoarx <kengarrido.legaspi@gmail.com>
2023-07-23 13:18:51 +0800

    set label and display for other required information

--- search_entry.py
+++ search_entry.py
@@ -54,18 +54,45 @@ class Read:
                     name = tkinter.Label(display_info, text="Full Name: " + str(data[x][0]))
                     name.pack(anchor='w')
                     # age
+                    age = tkinter.Label(display_info, text="Age: " + str(data[x][1]))
+                    age.pack(anchor='w')
                     # address
+                    address = tkinter.Label(display_info, text="Address: " + str(data[x][2]))
+                    address.pack(anchor='w')
                     # email
+                    email = tkinter.Label(display_info, text="Email: " + str(data[x][3]))
+                    email.pack(anchor='w')
                     # contact
+                    contact = tkinter.Label(display_info, text="Contact Number: " + str(data[x][4]))
+                    contact.pack(anchor='w')
                     # guardian name
+                    guardian_name = tkinter.Label(display_info, text="Guardian's Name: " + str(data[x][5]))
+                    guardian_name.pack(anchor='w')
                     # guardian contact
+                    guardian_contact = tkinter.Label(display_info, text="Guardian's Contact No.: " + str(data[x][6]))
+                    guardian_contact.pack(anchor='w')
                     # relationship
+                    relationship = tkinter.Label(display_info, text="Relationship: " + str(data[x][7]))
+                    relationship.pack(anchor='w')
                     # question 1
+                    vaccination_status = tkinter.Label(display_info, text="Vaccination(Yes/No): " + str(data[x][8]))
+                    vaccination_status.pack(anchor='w')
                     # question 2
+                    symptoms = tkinter.Label(display_info, text="Symptoms (Fever, Cough, Sore throat, Loss of taste/smell): " + str(data[x][9]))
+                    symptoms.pack(anchor='w')
                     # question 3
+                    exposure = tkinter.Label(display_info, text="Exposed? (Yes/No): " + str(data[x][10]))
+                    exposure.pack(anchor='w')
                     # question 4
+                    contact = tkinter.Label(display_info, text="Have been in contact to someone with COVID? (Yes/No): " + str(data[x][11]))
+                    contact.pack(anchor='w')
                     # question 5
+                    positive = tkinter.Label(display_info, text="Tested positive in COVID? (Yes/No): " + str(data[x][12]))
+                    positive.pack(anchor='w')
                     # question 6
+                    places = tkinter.Label(display_info, text="Visited places: " + str(data[x][13]))
+                    places.pack(anchor='w')
+                    print(data[x][0])
 
             # if the name is not found, display error message
 
